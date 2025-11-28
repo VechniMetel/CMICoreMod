e6cb81a96ad01610386e8406cb0c880d4e591a8e
Qi-Month <dy757410@163.com>
2025-11-29 00:45:22 +0800

    把过滤器爆破了!

---
+++ src/main/java/top/nebula/cmi/mixin/ClassFilterMixin.java
@@ -0,0 +1,20 @@
+package top.nebula.cmi.mixin;
+
+import dev.latvian.mods.kubejs.util.ClassFilter;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClassFilter.class)
+public abstract class ClassFilterMixin {
+	@Inject(method = "deny(Ljava/lang/String;)V", at = @At("HEAD"), cancellable = true)
+	public void cmi$cancelDenyString(String s, CallbackInfo info) {
+		info.cancel();
+	}
+
+	@Inject(method = "deny(Ljava/lang/Class;)V", at = @At("HEAD"), cancellable = true)
+	public void cmi$cancelDenyClass(Class<?> c, CallbackInfo info) {
+		info.cancel();
+	}
+}
