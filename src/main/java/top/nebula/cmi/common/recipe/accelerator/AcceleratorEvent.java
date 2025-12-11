2a24b275e5db2c558d0c8bdbf5129938141bef84
Qi-Month <dy757410@163.com>
2025-12-11 21:09:46 +0800

    回退了

--- src/main/java/top/nebula/cmi/common/recipe/accelerator/AcceleratorEvent.java
+++ src/main/java/top/nebula/cmi/common/recipe/accelerator/AcceleratorEvent.java
@@ -20,8 +20,6 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import top.nebula.cmi.CMI;
 
-import java.util.Optional;
-
 @SuppressWarnings("ALL")
 @Mod.EventBusSubscriber(modid = CMI.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class AcceleratorEvent {
@@ -50,98 +48,86 @@ public class AcceleratorEvent {
 		SimpleContainer container = new SimpleContainer(1);
 		container.setItem(0, item);
 
-		Optional<AcceleratorRecipe> recipeFound = level.getRecipeManager()
-				.getAllRecipesFor(AcceleratorRecipe.Type.INSTANCE)
-				.stream()
-				.filter((recipe) -> {
-					boolean input = recipe.inputs.stream()
-							.anyMatch((ingredient) -> {
-								return ingredient.test(item);
-							});
-					boolean target = state.is(recipe.targetBlock);
+		level.getRecipeManager()
+				.getRecipeFor(AcceleratorRecipe.Type.INSTANCE, container, level)
+				.ifPresent((recipe) -> {
+					int required = 24;
+					int matched = 0;
 
-					return input || target;
-				})
-				.findFirst();
+					for (int dx = -2; dx <= 2; dx++) {
+						for (int dz = -2; dz <= 2; dz++) {
 
-		recipeFound.ifPresent((recipe) -> {
-			int required = 24;
-			int matched = 0;
+							if (dx == 0 && dz == 0) {
+								continue;
+							}
 
-			for (int dx = -2; dx <= 2; dx++) {
-				for (int dz = -2; dz <= 2; dz++) {
-
-					if (dx == 0 && dz == 0) {
-						continue;
-					}
-
-					BlockPos checkPos = pos.offset(dx, 0, dz);
-					if (level.getBlockState(checkPos).is(recipe.targetBlock)) {
-						matched++;
-					}
-				}
-			}
-
-			if (matched != required) {
-				return;
-			}
-			RandomSource random = level.getRandom();
-
-			for (int dx = -2; dx <= 2; dx++) {
-				for (int dz = -2; dz <= 2; dz++) {
-					BlockPos targetPos = pos.offset(dx, 0, dz);
-
-					if (level.getBlockState(targetPos).is(recipe.targetBlock)) {
-						for (AcceleratorRecipe.OutputEntry out : recipe.outputs) {
-							BlockState outputBlock = out.block.defaultBlockState();
-
-							if (random.nextFloat() <= out.chance) {
-								// 破坏方块为了粒子效果和音效
-								level.destroyBlock(targetPos, false);
-								level.setBlock(targetPos, outputBlock, 3);
-								break;
+							BlockPos checkPos = pos.offset(dx, 0, dz);
+							if (level.getBlockState(checkPos).is(recipe.targetBlock)) {
+								matched++;
 							}
 						}
 					}
-				}
-			}
 
-			// 申必音效
-			level.playSound(
-					null,
-					pos.getX() + 0.5,
-					pos.getY() + 1.0,
-					pos.getZ() + 0.5,
-					AllSoundEvents.CRAFTER_CRAFT.getMainEvent(),
-					SoundSource.VOICE,
-					3,
-					1
-			);
-			// 申必粒子效果
-			for (int number = 0; number < 10; number++) {
-				double offsetX = (Math.random() - 0.5) * 0.5;
-				double offsetY = Math.random() * 0.5;
-				double offsetZ = (Math.random() - 0.5) * 0.5;
+					if (matched != required) {
+						return;
+					}
+					RandomSource random = level.getRandom();
 
-				sl.sendParticles(
-						ParticleTypes.HAPPY_VILLAGER,
-						pos.getX() + 0.5,
-						pos.getY() + 1.2,
-						pos.getZ() + 0.5,
-						1,
-						offsetX,
-						offsetY,
-						offsetZ,
-						0.02
-				);
-			}
+					for (int dx = -2; dx <= 2; dx++) {
+						for (int dz = -2; dz <= 2; dz++) {
+							BlockPos targetPos = pos.offset(dx, 0, dz);
 
-			if (!player.isCreative()) {
-				item.shrink(1);
-			}
+							if (level.getBlockState(targetPos).is(recipe.targetBlock)) {
+								for (AcceleratorRecipe.OutputEntry out : recipe.outputs) {
+									BlockState outputBlock = out.block.defaultBlockState();
 
-			event.setCanceled(true);
-			event.setCancellationResult(InteractionResult.SUCCESS);
-		});
+									if (random.nextFloat() <= out.chance) {
+										// 破坏方块为了粒子效果和音效
+										level.destroyBlock(targetPos, false);
+										level.setBlock(targetPos, outputBlock, 3);
+										break;
+									}
+								}
+							}
+						}
+					}
+
+					// 申必音效
+					level.playSound(
+							null,
+							pos.getX() + 0.5,
+							pos.getY() + 1.0,
+							pos.getZ() + 0.5,
+							AllSoundEvents.CRAFTER_CRAFT.getMainEvent(),
+							SoundSource.VOICE,
+							3,
+							1
+					);
+					// 申必粒子效果
+					for (int number = 0; number < 10; number++) {
+						double offsetX = (Math.random() - 0.5) * 0.5;
+						double offsetY = Math.random() * 0.5;
+						double offsetZ = (Math.random() - 0.5) * 0.5;
+
+						sl.sendParticles(
+								ParticleTypes.HAPPY_VILLAGER,
+								pos.getX() + 0.5,
+								pos.getY() + 1.2,
+								pos.getZ() + 0.5,
+								1,
+								offsetX,
+								offsetY,
+								offsetZ,
+								0.02
+						);
+					}
+
+					if (!player.isCreative()) {
+						item.shrink(1);
+					}
+
+					event.setCanceled(true);
+					event.setCancellationResult(InteractionResult.SUCCESS);
+				});
 	}
 }
