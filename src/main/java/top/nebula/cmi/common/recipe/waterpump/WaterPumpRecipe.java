260bf1d19a5f307575e605b6d83f2abe804ac7cc
Qi-Month <dy757410@163.com>
2025-12-21 23:11:39 +0800

    NotNull

--- src/main/java/top/nebula/cmi/common/recipe/waterpump/WaterPumpRecipe.java
+++ src/main/java/top/nebula/cmi/common/recipe/waterpump/WaterPumpRecipe.java
@@ -17,83 +17,84 @@ import org.jetbrains.annotations.Nullable;
 import slimeknights.mantle.data.loadable.common.FluidStackLoadable;
 
 public class WaterPumpRecipe implements Recipe<SimpleContainer> {
-    public final ResourceLocation id;
-    public final Ingredient input;
-    public final FluidStack water;
+	public final ResourceLocation id;
+	public final Ingredient input;
+	public final FluidStack water;
 
-    public WaterPumpRecipe(ResourceLocation id, Ingredient input,  FluidStack water) {
-        this.id = id;
-        this.input = input;
-        this.water = water;
-    }
+	public WaterPumpRecipe(ResourceLocation id, Ingredient input, FluidStack water) {
+		this.id = id;
+		this.input = input;
+		this.water = water;
+	}
 
-    @Override
-    public boolean matches(SimpleContainer container, Level pLevel) {
-        return false;
-    }
+	@Override
+	public boolean matches(@NotNull SimpleContainer container, @NotNull Level level) {
+		return false;
+	}
 
-    @Override
-    public ItemStack assemble(SimpleContainer pContainer, RegistryAccess pRegistryAccess) {
-        return ItemStack.EMPTY;
-    }
+	@Override
+	public @NotNull ItemStack assemble(@NotNull SimpleContainer container, @NotNull RegistryAccess access) {
+		return ItemStack.EMPTY;
+	}
 
-    @Override
-    public boolean canCraftInDimensions(int pWidth, int pHeight) {
-        return false;
-    }
+	@Override
+	public boolean canCraftInDimensions(int width, int height) {
+		return false;
+	}
 
-    @Override
-    public boolean isSpecial() {
-        return true;
-    }
+	@Override
+	public boolean isSpecial() {
+		return true;
+	}
 
-    @Override
-    public ItemStack getResultItem(RegistryAccess pRegistryAccess) {
-        return ItemStack.EMPTY;
-    }
+	@Override
+	public @NotNull ItemStack getResultItem(@NotNull RegistryAccess access) {
+		return ItemStack.EMPTY;
+	}
 
-    @Override
-    public ResourceLocation getId() {
-        return this.id;
-    }
+	@Override
+	public @NotNull ResourceLocation getId() {
+		return this.id;
+	}
 
-    @Override
-    public RecipeSerializer<?> getSerializer() {
-        return Serializer.INSTANCE;
-    }
+	@Override
+	public @NotNull RecipeSerializer<?> getSerializer() {
+		return Serializer.INSTANCE;
+	}
 
-    @Override
-    public RecipeType<?> getType() {
-        return Type.INSTANCE;
-    }
+	@Override
+	public @NotNull RecipeType<?> getType() {
+		return Type.INSTANCE;
+	}
 
-    public static class Type implements RecipeType<WaterPumpRecipe> {
-        private Type() {}
+	public static class Type implements RecipeType<WaterPumpRecipe> {
+		private Type() {
+		}
 
-        public static final WaterPumpRecipe.Type INSTANCE = new WaterPumpRecipe.Type();
-    }
+		public static final WaterPumpRecipe.Type INSTANCE = new WaterPumpRecipe.Type();
+	}
 
-    public static class Serializer implements RecipeSerializer<WaterPumpRecipe> {
-        public static final WaterPumpRecipe.Serializer INSTANCE = new WaterPumpRecipe.Serializer();
+	public static class Serializer implements RecipeSerializer<WaterPumpRecipe> {
+		public static final WaterPumpRecipe.Serializer INSTANCE = new WaterPumpRecipe.Serializer();
 
-        @Override
-        public @NotNull WaterPumpRecipe fromJson(@NotNull ResourceLocation id, @NotNull JsonObject json) {
-            FluidStack stack = FluidStackLoadable.REQUIRED_BUCKET.getIfPresent(json, "water");
-            Ingredient ingredient  = Ingredient.fromJson(json.getAsJsonObject("input"));
-            return new WaterPumpRecipe(id, ingredient, stack);
-        }
+		@Override
+		public @NotNull WaterPumpRecipe fromJson(@NotNull ResourceLocation id, @NotNull JsonObject json) {
+			FluidStack stack = FluidStackLoadable.REQUIRED_BUCKET.getIfPresent(json, "water");
+			Ingredient ingredient = Ingredient.fromJson(json.getAsJsonObject("input"));
+			return new WaterPumpRecipe(id, ingredient, stack);
+		}
 
-        @Override
-        public @Nullable WaterPumpRecipe fromNetwork(@NotNull ResourceLocation id, @NotNull FriendlyByteBuf buf) {
-            FluidStack stack = FluidStack.readFromPacket(buf);
-            Ingredient ingredient = Ingredient.fromNetwork(buf);
-            return new WaterPumpRecipe(id, ingredient, stack);
-        }
+		@Override
+		public @Nullable WaterPumpRecipe fromNetwork(@NotNull ResourceLocation id, @NotNull FriendlyByteBuf buf) {
+			FluidStack stack = FluidStack.readFromPacket(buf);
+			Ingredient ingredient = Ingredient.fromNetwork(buf);
+			return new WaterPumpRecipe(id, ingredient, stack);
+		}
 
-        @Override
-        public void toNetwork(@NotNull FriendlyByteBuf buf, @NotNull WaterPumpRecipe recipe) {
-            FluidStack.readFromPacket(buf);
-            recipe.input.toNetwork(buf);
-        }
-    }
+		@Override
+		public void toNetwork(@NotNull FriendlyByteBuf buf, @NotNull WaterPumpRecipe recipe) {
+			FluidStack.readFromPacket(buf);
+			recipe.input.toNetwork(buf);
+		}
+	}
 }
